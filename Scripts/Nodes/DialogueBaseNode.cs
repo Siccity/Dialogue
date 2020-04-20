e649be210a3e7bd1f2231c3292575d37e254e208
Thor Brigsted <thorbrigsted@gmail.com>
2020-04-20 10:56:01 +0200

    Removed base node constraint to fix Answer outputs

--- Scripts/Nodes/DialogueBaseNode.cs
+++ Scripts/Nodes/DialogueBaseNode.cs
@@ -2,7 +2,7 @@
 
 namespace Dialogue {
 	public abstract class DialogueBaseNode : Node {
-		[Input(backingValue = ShowBackingValue.Never, typeConstraint = TypeConstraint.Inherited)] public DialogueBaseNode input;
+		[Input(backingValue = ShowBackingValue.Never)] public DialogueBaseNode input;
 		[Output(backingValue = ShowBackingValue.Never)] public DialogueBaseNode output;
 
 		abstract public void Trigger();
